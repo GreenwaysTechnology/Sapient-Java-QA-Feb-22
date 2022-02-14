3f8954a63bed892025faed5c5f83cbd5a45aea54
Subramanian Murugan <sasubramanian_md@hotmail.com>
2022-02-14 11:02:09 +0530

    java introduction

---
+++ java/javaapps/src/main/java/com/sapient/core/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.sapient.core;
+
+public class HelloWorld {
+    //main method
+    public static void main(String args[]) {
+        //print some text on console
+        System.out.println("Hello World Java");
+    }
+}
